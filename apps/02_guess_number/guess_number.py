af94583fa61dd18d6835534274f9791ffb1508b8
Philly Dilly <tonemontone@gmail.com>
2016-07-16 17:54:40 -0400

    first iteration of guess the number

---
+++ apps/02_guess_number/guess_number.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+
+import random
+
+print('-----------------------------')
+print('  Guess that number game     ')
+print('-----------------------------')
+print()
+
+the_number = random.randint(0,100)
+guess = -1
+
+while guess != the_number:
+
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
+
+    if guess > the_number:
+        print("too high")
+    elif guess < the_number:
+        print("too low")
+    else:
+        print("Good guess!")
+
+print('done')
