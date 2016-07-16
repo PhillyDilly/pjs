872303789dfa7b06303151d10f89ddab28b55fff
Philly Dilly <tonemontone@gmail.com>
2016-07-16 16:44:21 -0400

    Completed first project

---
+++ apps/01_hello_world/hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+print("-------------------------------")
+print("		Hello App	      ")
+print("-------------------------------")
+
+
+print("What is your name? ")
+
+your_name = input()
+
+print("Hello " + your_name + ", welcome to PJS.")
